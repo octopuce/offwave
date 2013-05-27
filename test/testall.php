f7fb04a49fae484d99573c615684b8d8b2f9e074
Benjamin Sonntag <benjamin@sonntag.fr>
2013-05-27 21:11:11 +0200

    Adding test in test environment

--- test/testall.php
+++ test/testall.php
@@ -1,6 +1,24 @@
 #!/usr/bin/env php
 <?php
 
-   // TODO : launch test on all cms and version in all folders and subfolders from __DIR__
+// Loads the Offwave scanner
+require_once __DIR__."/../lib/Offwave/Scanner.php";
+require_once __DIR__."/../lib/Offwave/Exception.php";
+require_once __DIR__."/../lib/Offwave/Agents/Abstract.php";
+
+// Starts the scanner
+$offwave_scanner = new Offwave_Scanner();
+
+// Scan the test environment
+$path = __DIR__."/testenv";
+
+Offwave_Scanner::$do_debug = FALSE;
+
+$offwave_result = $offwave_scanner->scan( $path, 2);
+//or maybe Offwave_Scanner::DEPTH_INFINITE instead of 2 ?
+
+//print_r( Offwave_Scanner::debug());
+
+print_r($offwave_result);
 
 ?>
