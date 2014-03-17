ce53102d94b73980d1980d955e2a15c09b06771b
alban <alban@octopuce.fr>
2014-03-17 16:57:41 +0100

    [fix] spip identify version by cvs.revision

--- lib/Offwave/Agents/Cms/Spip.php
+++ lib/Offwave/Agents/Cms/Spip.php
@@ -14,7 +14,21 @@ class Offwave_Agents_Cms_Spip extends Offwave_Agents_Abstract {
 //    }
     
   public function identifyVersion($path,$parameters) {      
-    return $parameters;
+      
+      $svn_revision_file            = $path."/svn.revision";
+      if(is_file($svn_revision_file)){
+          $file_content = file_get_contents($svn_revision_file);
+          preg_match("/spip\-([\d\.\w]*)/", $file_content, $matches);
+          if( isset($matches[1]) && !is_null($matches[1])){
+              $version = $matches[1];
+              return array(
+                "application"   => $this->getApplicationName(),
+                "version"       => $version
+                );
+          }
+      }
+      
+      return $parameters;
     }
     
     
