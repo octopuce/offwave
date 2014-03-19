54b21c5dcc7499a1fb9a519175cef112221e6e6d
alban <alban@octopuce.fr>
2014-03-19 23:29:49 +0100

    [fix] Joomla + Typo3 version identification

--- lib/Offwave/Agents/Cms/Joomla.php
+++ lib/Offwave/Agents/Cms/Joomla.php
@@ -2,5 +2,23 @@
 
 class Offwave_Agents_Cms_Joomla extends Offwave_Agents_Abstract {
     
+    public function identifyVersion($path,$parameters) {      
+        $matchList                  = array(
+            $path."/CHANGELOG.php"   => "(.*).*Released ",
+        );
+        foreach($matchList as $file => $pattern){
+            if(is_file($file)){
+                $file_content                 = file_get_contents($file);
+                preg_match($pattern, $file_content, $matches);
+                if( isset($matches[1]) && !is_null($matches[1])){
+                    return array(
+                      "application"   => $this->getApplicationName(),
+                      "version"       => $matches[1]
+                      );
+                }
+            }
+        }
+      return $parameters;
+    }
 
 }
--- lib/Offwave/Agents/Cms/Typo3.php
+++ lib/Offwave/Agents/Cms/Typo3.php
@@ -3,5 +3,23 @@
 class Offwave_Agents_Cms_Typo3 extends Offwave_Agents_Abstract {
     
     
-    
+    public function identifyVersion($path,$parameters) {      
+        $matchList                  = array(
+            $path."/t3lib/config_default.php"   => "TYPO_VERSION = (.*)",
+            $path."/ChangeLog"                  => "Release of TYPO3 (.*)"
+        );
+        foreach($matchList as $file => $pattern){
+            if(is_file($file)){
+                $file_content                 = file_get_contents($file);
+                preg_match($pattern, $file_content, $matches);
+                if( isset($matches[1]) && !is_null($matches[1])){
+                    return array(
+                      "application"   => $this->getApplicationName(),
+                      "version"       => $matches[1]
+                      );
+                }
+            }
+        }
+      return $parameters;
+    }
 }
