0bcc596f897c2f5a415a2139407b4f1fd4ecaad1
alban <alban@octopuce.fr>
2014-03-19 19:19:01 +0100

    [enh] Adds PhpBB version identification

--- lib/Offwave/Agents/Forums/Phpbb.php
+++ lib/Offwave/Agents/Forums/Phpbb.php
@@ -1,4 +1,29 @@
 <?php
 
 class Offwave_Agents_Forums_Phpbb extends Offwave_Agents_Abstract {
+    
+        
+    /**
+     * 
+     * Attempts to identify using the version.php
+     * 
+     * @param type $path
+     * @param type $parameters
+     * @return type
+     */
+    public function identifyVersion($path,$parameters){
+        
+        $version_file                   = $path."/database_update.php";
+        if(is_readable($version_file)){
+            $file_content               = file_get_contents($version_file);
+            preg_match("/^\$updates_to_version = '(.*)';/",$file_content,$matches);
+            if( isset($matches[1])){
+                return array(
+                "application"   => $this->getApplicationName(),
+                "version"       => $matches[1]
+            );
+          }
+      }
+      return $parameters;
+    }
 }
