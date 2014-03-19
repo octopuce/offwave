8dec8200535de07a2f487ff6d9621b56227df16f
alban <alban@octopuce.fr>
2014-03-19 15:03:56 +0100

    [enh] Adds Wordpress identifyVersion

--- lib/Offwave/Agents/Cms/Wordpress.php
+++ lib/Offwave/Agents/Cms/Wordpress.php
@@ -3,5 +3,32 @@
 class Offwave_Agents_Cms_Wordpress extends Offwave_Agents_Abstract {
     
     
+    /**
+     * 
+     * Attempts to identify using the version.php
+     * 
+     * @param type $path
+     * @param type $parameters
+     * @return type
+     */
+    public function identityVersion($path,$parameters){
+        
+        $version_file                   = $path."/wp-includes/version.php";
+        if(is_readable($version_file)){
+            $file_content               = file_get_contents($version_file);
+            preg_match("/wp_version = '(.*)';/", $file_content,$matches);
+            if( isset($matches[1])){
+                return array(
+                "application"   => $this->getApplicationName(),
+                "version"       => $matches[1]
+            );
+          }
+      }
+      return $parameters;
+    }
     
+    
+  public function identifyModules($path,$parameters) {
+    return $parameters;
+  }
 }
