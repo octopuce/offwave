987b812ddc97f3bba7b1b2cd620d6788ca378784
alban <alban@octopuce.fr>
2014-03-19 17:46:27 +0100

    [fix] Messy code

--- lib/Offwave/Agents/Cms/Joomla.php
+++ lib/Offwave/Agents/Cms/Joomla.php
@@ -2,19 +2,5 @@
 
 class Offwave_Agents_Cms_Joomla extends Offwave_Agents_Abstract {
     
-    
-//    public function identifyApplication($path){
-//		Alors joomla a un arbre comme ça en 1.0+
-//		Mais joomla a un arbre comme ça en 1.5+
-//    }
-    
-  public function identifyVersion($path,$parameters) {      
-    return $parameters;
-    }
-    
-    
-  public function identifyModules($path,$parameters) {
-    return $parameters;
-  }
-    
+
 }
--- lib/Offwave/Agents/Cms/Spip.php
+++ lib/Offwave/Agents/Cms/Spip.php
@@ -1,17 +1,7 @@
 <?php
 
 class Offwave_Agents_Cms_Spip extends Offwave_Agents_Abstract {
-    
-    
-//    public function identifyApplication($path){
-		
-//		Alors spip a un arbre comme ça en 1.0+
-//		
-//		Mais spip a un arbre comme ça en 1.5+
-//		
-//		Oui et la DB est
-//		
-//    }
+
     
   public function identifyVersion($path,$parameters) {      
       
@@ -31,9 +21,5 @@ class Offwave_Agents_Cms_Spip extends Offwave_Agents_Abstract {
       return $parameters;
     }
     
-    
-  public function identifyModules($path,$parameters) {
-    return $parameters;
-  }
     
 }
